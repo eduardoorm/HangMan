e07c3335f2a7e81ea1543329508d5436b04a4392
eduardo ormeño <eduardoorm79@gmail.com>
2021-09-03 19:19:31 -0500

    finished mockup

--- main.py
+++ main.py
@@ -7,9 +7,42 @@ root = Tk()
 root.title("Juego del Ahorcado")
 root.geometry("800x600")
 
-stringEntered = tk.Label(background="green",width=60,text="BIENVENIDO AL JUEGO DEL AHORCADO",padx=20,pady=20,font=("Courier",20)).pack()
-btnLabel = tk.Button(text="Empezar el juego",pady=10,padx=10).pack()
 
+
+def StartPlay():
+    btnStart.pack_forget()
+    entryCadena.pack()
+    btnAccept.pack()
+    string.pack()
+
+def acceptString():
+    btnAccept.pack_forget()
+    entryCadena.pack_forget()
+    string.pack_forget()
+    EnteredChar()
+
+def EnteredChar():
+    lblEnterChar.pack()
+    txtEnterChar.pack()
+    btnIntent.pack()
+
+
+
+string = tk.Label(text="Ingrese un texto")
+txtCadena= StringVar()
+entryCadena = tk.Entry(textvariable=txtCadena)
+
+btnAccept = tk.Button(text="accept",command=acceptString)
+
+lblEnterChar = tk.Label(text="Ingrese una letra")
+char = StringVar()
+txtEnterChar =  tk.Entry(textvariable=char)
+btnIntent = tk.Button(text="Buscar")
+
+stringEntered = tk.Label(background="green",width=60,text="BIENVENIDO AL JUEGO DEL AHORCADO",padx=20,pady=20,font=("Courier",20)).pack()
+btnStart = tk.Button(text="Empezar el juego",pady=10,padx=10,command=StartPlay)
+
+btnStart.pack()
 
 root.mainloop()
 
